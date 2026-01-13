f0136735ad07491a3a56224b98ba93f3792b64fe
sui <1202ayasnppk@gmail.com>
2026-01-14 08:32:57 +0900

    fix: 拡張機能のベース設定作成

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,24 @@
+import { crx, defineManifest } from '@crxjs/vite-plugin';
 import tailwindcss from '@tailwindcss/vite';
 import react from '@vitejs/plugin-react-swc';
 import path from 'path';
 import { defineConfig } from 'vite';
 
+const manifest = defineManifest({
+  manifest_version: 3,
+  name: 'テスト拡張機能',
+  version: '0.0.1',
+  description:
+    '拡張機能のオプションページを開いてボタンを押すと、コンソールにメッセージが表示されます。',
+  host_permissions: ['http://localhost:5173/*'],
+  action: {
+    default_popup: 'index.html',
+  },
+});
+
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [react(), tailwindcss(), crx({ manifest })],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
